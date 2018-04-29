f2f6d3e3b79ecd4d23050d665ac9476be0fe2247
nikolay <versus73@mail.ru>
2018-04-29 23:57:58 +0400

    first commit

---
+++ index.php
@@ -0,0 +1,103 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>alexandra</title>
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="css/index.css">
+</head>
+<body>
+	<header>
+		<div class="header-img">
+			<div class="header-img-fon"></div>
+		</div>
+		<div class="header-logo">
+			Alexandra
+			<div class="header-logoBlock"></div>
+		</div>
+		<div class="header-logoText">
+			1.6 and 1.7 Responsive Muti-Purpose Prestashop Themes
+		</div>
+		<div class="header-block">
+			<div class="header-block-screen">
+				<div class="header-block-screenImg">
+					<img src="image/bookImg.jpg" alt="">
+				</div>
+			</div>
+			<div class="header-block-top">
+				<div class="header-block-topBlock1">
+					<div class="header-block-topBlockMargin"></div>
+				</div>
+				<div class="header-block-topBlock2"></div>
+			</div>
+		</div>
+	</header>
+	<div class="container page">
+		<div class="col-sm-12">
+			<div class="page-buttons">
+				<a href="#">
+					<button class="page-buttons-doc col-sm-5">
+                        Documentation
+                    </button>
+				</a>
+				<a href="#">
+					<button class="page-buttons-ourChannel col-sm-5">
+                        <span></span>
+                        Our channel
+                    </button>
+				</a>
+			</div>
+			<div class="page-theme">
+				<div class="clearfix col-sm-6">
+					<div class="page-block">
+						<a href="#">
+							<div class="page-block-img">
+								<img src="image/theme1.jpg" alt="">
+								<div class="page-block-imgFon"></div>
+							</div>
+						</a>
+						<div class="page-block-title">
+                            Home page
+                        </div>
+						<div class="page-block-value">
+                            1.6
+                        </div>
+					</div>
+				</div>
+				<div class="clearfix col-sm-6">
+					<div class="page-block">
+						<a href="#">
+							<div class="page-block-img">
+								<img src="image/theme1.jpg" alt="">
+								<div class="page-block-imgFon"></div>
+							</div>
+						</a>
+						<div class="page-block-title">
+                            Home page
+                        </div>
+						<div class="page-block-value">
+                            1.7
+                        </div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<footer>
+		<div class="footer">
+			<div class="footer-title">
+				Start creating your website
+			</div>
+			<a href="#">
+				<button class="footer-botton">
+					Purcharse now
+				</button>
+			</a>
+		</div>
+		<div class="footerTop">
+			Create by prestapro
+		</div>
+	</footer>
+</body>
+</html>
