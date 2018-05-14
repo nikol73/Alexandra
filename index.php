6b3b2b294edf4b28420cb4f1f0cae2b7e1662224
versus73@mail.ru <versus73@mail.ru>
2018-05-15 00:25:21 +0400

    html5

--- index.php
+++ index.php
@@ -1,99 +1,101 @@
 <!DOCTYPE html>
 <html xml:lang="en" lang="en">
 	<head>
-		<meta http-equiv="Content-Type" content="text/html/xhtml; charset=utf-8" />
+		<meta http-equiv="Content-Type" content="text/xhtml; charset=utf-8" />
 	    <meta name="viewport" content="width=device-width, initial-scale=1">
-		<title>alexandra</title>
+		<title>Responsive MutiPurpose Prestashop Themes, creating your website</title>
 		<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css" media="all">
 		<link rel="stylesheet" type="text/css" href="css/index.css" media="all">
 	</head>
-	<body itemscope itemtype="http://schema.org/Action">
-		<header>
-			<div class="header-img">
-				<div class="header-img-fon"></div>
-			</div>
-			<div class="header-logo">
-				<span itemprop="name">
-					Alexandra
-				</span>
-				<div class="header-logoBlock"></div>
-			</div>
-			<div itemprop="description" class="header-logoText">
-				1.6 and 1.7 Responsive Muti-Purpose Prestashop Themes
-			</div>
-			<div class="header-block">
-				<div class="header-block-screen">
-					<div class="header-block-screenImg">
-						<img itemprop="image" src="image/bookImg.jpg" alt="home page image bookImg" longdesc="bookImg.html">
+	<body>
+		<main itemscope itemtype="http://schema.org/Action">
+			<header>
+				<nav>
+					<div class="header-img">
+						<div class="header-img-fon"></div>
 					</div>
-				</div>
-				<div class="header-block-top">
-					<div class="header-block-topBlock1">
-						<div class="header-block-topBlockMargin"></div>
+					<div class="header-logo">
+						<span itemprop="name">
+							Alexandra
+						</span>
+						<div class="header-logoBlock"></div>
 					</div>
-					<div class="header-block-topBlock2"></div>
-				</div>
-			</div>
-		</header>
-		<div class="container page">
-			<div class="col-sm-12">
-				<div class="page-buttons">
-					<button class="page-buttons-doc col-sm-5" onclick="javascript:window.location='index.php'">
-						Documentation
-					</button>
-					<button class="page-buttons-ourChannel col-sm-5" onclick="javascript:window.location='index.php'">
-						<span></span>
-						Our channel
-					</button>
-				</div>
-				<div class="page-theme">
-					<div class="clearfix col-sm-6">
-						<div itemprop="object" itemscope itemtype="http://schema.org/Action" class="page-block">
-							<a href="index.php">
-								<div class="page-block-img">
-									<img itemprop="image" src="image/theme1.jpg" alt="home page 1.6 theme1" longdesc="theme1.html">
-									<div class="page-block-imgFon"></div>
-								</div>
-							</a>
-							<div itemprop="name" class="page-block-title">
-								Home page
-							</div>
-							<div itemprop="identifier" class="page-block-value">
-								1.6
-							</div>
+					<p itemprop="description" class="header-logoText">
+						1.6 and 1.7 Responsive Muti-Purpose Prestashop Themes
+					</p>
+				</nav>
+				<div class="header-top header-block">
+					<div class="header-block-screen">
+						<div class="header-block-screenImg">
+							<img itemprop="image" src="image/book.jpg" alt="screen book" longdesc="book.html"/>
 						</div>
 					</div>
-					<div class="clearfix col-sm-6">
-						<div itemprop="object" itemscope itemtype="http://schema.org/Action" class="page-block">
-							<a href="index.php">
-								<div class="page-block-img">
-									<img itemprop="image" src="image/theme1.jpg" alt="home page 1.7 theme1" longdesc="theme1.html">
-									<div class="page-block-imgFon"></div>
-								</div>
-							</a>
-							<div itemprop="name" class="page-block-title">
-								Home page
-							</div>
-							<div itemprop="identifier" class="page-block-value">
-								1.7
-							</div>
+					<div class="header-block-top">
+						<div class="header-block-topBlock1">
+							<div class="header-block-topBlockMargin"></div>
 						</div>
+						<div class="header-block-topBlock2"></div>
 					</div>
 				</div>
-			</div>
-		</div>
-		<footer>
-			<div class="footer">
-				<div itemprop="description" class="footer-title">
-					Start creating your website
+			</header>
+			<section class="container page">
+				<div class="col-sm-12">
+					<nav>
+						<div class="page-buttons">
+							<button class="page-buttons-doc col-sm-5" onclick="javascript:window.location='index.php'">
+								Documentation
+							</button>
+							<button class="page-buttons-ourChannel col-sm-5" onclick="javascript:window.location='index.php'">
+								<span></span>
+								Our channel
+							</button>
+						</div>
+					</nav>
+					<section class="page-theme">
+						<article class="clearfix col-sm-6">
+							<div itemprop="object" itemscope itemtype="http://schema.org/Action" class="page-block">
+								<div class="page-block-img" onclick="javascript:window.location='index.php'">
+									<img itemprop="image" src="image/onetheme.jpg" alt="onetheme" longdesc="onetheme.html"/>
+									<div class="page-block-imgFon"></div>
+								</div>
+								<h1 itemprop="name" class="page-block-title">
+									Home page
+								</h1>
+								<ul itemprop="identifier" class="page-block-value">
+									<li>1.6</li>
+								</ul>
+							</div>
+						</article>
+						<article class="clearfix col-sm-6">
+							<div itemprop="object" itemscope itemtype="http://schema.org/Action" class="page-block">
+								<div class="page-block-img" onclick="javascript:window.location='index.php'">
+									<img itemprop="image" src="image/twotheme.jpg" alt="twotheme" longdesc="twotheme.html"/>
+									<div class="page-block-imgFon"></div>
+								</div>
+								<h1 itemprop="name" class="page-block-title">
+									Home page
+								</h1>
+								<ul itemprop="identifier" class="page-block-value">
+									<li>1.7</li>
+								</ul>
+							</div>
+						</article>
+					</section>
 				</div>
-				<button class="footer-botton" onclick="javascript:window.location='index.php'">
-					Purcharse now
-				</button>
-			</div>
-			<div itemprop="disambiguatingDescription" class="footerTop">
-				Create by prestapro
-			</div>
-		</footer>
+			</section>
+			<footer>
+				<div class="footer">
+					<h5 itemprop="description" class="footer-title">
+						Start creating your website
+					</h5>
+					<button class="footer-botton" onclick="javascript:window.location='index.php'">
+						Purcharse now
+					</button>
+				</div>
+				<p itemprop="disambiguatingDescription" class="footerTop">
+					Create by prestapro
+				</p>
+			</footer>
+		</main>
 	</body>
 </html>
